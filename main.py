052f83b2cbfb0e7e9442b3e219fbfd8904f21c2a
as.grebennikov <as.grebennikov@tensor.ru>
2017-08-12 14:16:54 +0700

    Sample PyQt code

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 import sys
+from PyQt5.QtWidgets import QApplication, QWidget
+import PyQt5.QtCore.QThread
 
 
 if __name__ == '__main__':
-    pass
+    app = QApplication(sys.argv)
+
+    w = QWidget()
+    w.resize(250, 150)
+    w.move(300, 300)
+    w.setWindowTitle('Simple')
+    w.show()
+
+    sys.exit(app.exec_())
+
+    # move to thead??
