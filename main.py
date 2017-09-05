6bc0dd4e23a72e0b71fe3f9e2d0d3c818e7f81fc
AlexEx <alexex@list.ru>
2017-09-06 00:54:37 +0700

    added background worker

--- main.py
+++ main.py
@@ -1,17 +1,28 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QWidget
-import PyQt5.QtCore.QThread
+from PyQt5.QtCore import QThread
+import workers
+
+
+class MainWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.__worker = workers.Worker()
+        self.__thread = QThread()
+
+        self.__worker.moveToThread(self.__thread)
+        self.__thread.started.connect(self.__worker.do_task)
+        self.__thread.start()
+
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
 
-    w = QWidget()
+    w = MainWindow()
     w.resize(250, 150)
     w.move(300, 300)
-    w.setWindowTitle('Simple')
+    w.setWindowTitle('ev_simulator')
     w.show()
 
     sys.exit(app.exec_())
-
-    # move to thead??
